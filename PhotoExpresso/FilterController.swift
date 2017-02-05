a18653f45e88dfab91a2ca26c9e41974a1cd249b
Romain <romain@scaleflex.cloud>
2017-02-05 20:37:42 +0100

    changement de vue

---
+++ PhotoExpresso/FilterController.swift
@@ -0,0 +1,17 @@
+//
+//  FilterController.swift
+//  PhotoExpresso
+//
+//  Created by Romain on 03/02/17.
+//  Copyright © 2017 Romain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilterController : UIViewController {
+    @IBAction func NewFilter(_ sender: UIButton) {
+    }
+    @IBOutlet var ImageView: UIImageView!
+
+}
